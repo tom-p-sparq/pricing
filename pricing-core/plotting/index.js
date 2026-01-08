c2dbc9ddb7293c137c47156724ca5a80abc6ff7b
tom-p-sparq (Tom Prescott) <tom.prescott@datasparq.ai>
2026-01-08 16:42:01 +0000

    One more explicit import

--- pricing-core/plotting/index.js
+++ pricing-core/plotting/index.js
@@ -4,7 +4,7 @@ import * as d3 from 'd3'
 /**
  * Generates a plot definition for a given demand model using Observable Plot.
  *
- * @param {import('../conversion/base').BaseDemandModel} model An instance of a class that extends `BaseDemandModel`.
+ * @param {import('../conversion/base.js').BaseDemandModel} model An instance of a class that extends `BaseDemandModel`.
  * @param {object} [options] Optional configuration for the plot.
  * @param {number} [options.max_price=400] The maximum price to plot on the x-axis.
  * @param {string} [options.title] The main title for the plot.
