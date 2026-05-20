6ffb08cef23bf3e6733729558924bb26ae175571
tom-p-sparq (Tom Prescott) <tom.prescott@datasparq.ai>
2026-05-20 11:48:31 +0100

    Rigorous generator

--- pricing-core/fitting/fit.js
+++ pricing-core/fitting/fit.js
@@ -11,13 +11,13 @@ import { logLikelihood } from "./likelihoods.js"
  * @param {Object} options
  * @param {number} [options.epsilon=1e-5] The convergence threshold
  * @param {number} [options.batchSize=100] The number of steps per iteration
- * @returns {Generator<BaseDemandModel, BaseDemandModel, void>} A generator that yields the model at each step of the fitting process.
+ * @returns {Generator<BaseDemandModel, void, void>} A generator that yields the model at each step of the fitting process.
  */
 export function* fit(model, optimiser, data, { epsilon = 1e-5, batchSize = 100 }) {
     const numPoints = data.length;
     const modelClass = Object.getPrototypeOf(model).constructor
     if (numPoints == 0) {
-        return model
+        yield model
     }
     if (numPoints == 1) {
         const { price, looks, books } = data[0]
