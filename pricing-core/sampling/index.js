2e5bf785c234ffebecd1be71726ccd314e0b15f0
tom-p-sparq (Tom Prescott) <tom.prescott@datasparq.ai>
2026-05-21 14:53:45 +0100

    Fix api

--- pricing-core/sampling/index.js
+++ pricing-core/sampling/index.js
@@ -2,5 +2,6 @@ export * as distributions from './distributions/index.js';
 export * as steps from './steps/index.js'
 export { Proposal } from './proposals.js'
 export { Prior } from './priors.js'
-export { mh } from './mcmc.js'
 export { samplePrior } from './iid.js'
+export { mh } from './mcmc.js'
+export { particleFilter } from './particleFilter.js'
