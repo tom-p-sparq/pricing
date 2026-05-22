667f1e762628c90c86c6623fcf83b299db792b77
tom-p-sparq (Tom Prescott) <tom.prescott@datasparq.ai>
2026-05-22 16:35:08 +0100

    Make a seedable RNG

--- pricing-core/sampling/index.js
+++ pricing-core/sampling/index.js
@@ -1,3 +1,4 @@
+export { createRng } from './rng.js'
 export * as distributions from './distributions/index.js';
 export * as steps from './steps/index.js'
 export { Proposal } from './proposals.js'
